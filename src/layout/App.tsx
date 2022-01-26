c02ee7da0a30eaad2b09f0ff849629e2bcd0bddf
kula <kula@live.com>
2022-01-26 18:29:23 +0800

    feat: read graphql datasource

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,14 +1,13 @@
 import CreateChannelPage from '@/page/CreateChannel'
 import CreateMessagePage from '@/page/CreateMessage'
 import FeedPage from '@/page/Feed'
-import IndexPage from '@/page/Index'
 import ListChannelPage from '@/page/ListChannel'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<IndexPage />} />
+      <Route path="/" element={<FeedPage />} />
       <Route path="/createchannel" element={<CreateChannelPage />} />
       <Route path="/listchannel" element={<ListChannelPage />} />
       <Route path="/createmessage" element={<CreateMessagePage />} />
--- src/page/Feed.tsx
+++ src/page/Feed.tsx
@@ -1,8 +1,52 @@
 import { HeaderPage } from '@/layout/header'
 import { NavPage } from '@/layout/navbar'
-import { AppShell } from '@mantine/core'
+import { AppShell, Card, Group, List, Skeleton, Text } from '@mantine/core'
+import { gql, request } from 'graphql-request'
+import { useQuery } from 'react-query'
+
+const endpoint = 'https://api.thegraph.com/subgraphs/name/kulame/npns'
 
 export default function FeedPage() {
+  const { isLoading, isError, error, data } = useQuery('feeds', async () => {
+    return await request(
+      endpoint,
+      gql`
+        query {
+          messages(first: 5, orderBy: id, orderDirection: desc) {
+            id
+            height
+            title
+            content
+          }
+        }
+      `
+    )
+  })
+
+  if (isLoading) {
+    return <Skeleton height={50} circle mb="xl" />
+  }
+
+  if (isError) {
+    return <span>Error:{error instanceof Error ? error.message : ''}</span>
+  }
+
+  const feeds_view = data.messages.map(
+    (feed: Record<string, string>, index: number) => {
+      return (
+        <List.Item key={index}>
+          <Card shadow="sm" padding="lg">
+            <Group position="apart">
+              <Text weight={500}>{feed.title}</Text>
+            </Group>
+
+            <Text>{feed.content}</Text>
+          </Card>
+        </List.Item>
+      )
+    }
+  )
+
   return (
     <AppShell
       padding="md"
@@ -17,7 +61,7 @@ export default function FeedPage() {
         }
       })}
     >
-      hello
+      <List>{feeds_view}</List>
     </AppShell>
   )
 }
