671ae03868ec5fa404fdd3faf1fa0b924eae48b6
kula <kula@live.com>
2022-01-27 21:55:53 +0800

    feat: 如果未登陆只展示信息流

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,3 +1,4 @@
+import { wallet } from '@/near/Account'
 import { Button, Navbar } from '@mantine/core'
 import { useNavigate } from 'react-router-dom'
 
@@ -5,44 +6,58 @@ export function NavPage() {
   const navigate = useNavigate()
   return (
     <Navbar height={600} padding="xs" width={{ base: 120 }}>
-      <Navbar.Section>
-        <Button
-          className="w-full"
-          variant="outline"
-          onClick={() => {
-            navigate('/feed')
-          }}
-        >
-          信息流
-        </Button>
-        <Button
-          className="w-full"
-          variant="outline"
-          onClick={() => {
-            navigate('/listchannel')
-          }}
-        >
-          频道列表
-        </Button>
-        <Button
-          className="w-full"
-          variant="outline"
-          onClick={() => {
-            navigate('/createchannel')
-          }}
-        >
-          创建频道
-        </Button>
-        <Button
-          className="w-full"
-          variant="outline"
-          onClick={() => {
-            navigate('/createmessage')
-          }}
-        >
-          创建消息
-        </Button>
-      </Navbar.Section>
+      {wallet.isSignedIn() ? (
+        <Navbar.Section>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => {
+              navigate('/feed')
+            }}
+          >
+            信息流
+          </Button>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => {
+              navigate('/listchannel')
+            }}
+          >
+            频道列表
+          </Button>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => {
+              navigate('/createchannel')
+            }}
+          >
+            创建频道
+          </Button>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => {
+              navigate('/createmessage')
+            }}
+          >
+            创建消息
+          </Button>
+        </Navbar.Section>
+      ) : (
+        <Navbar.Section>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => {
+              navigate('/feed')
+            }}
+          >
+            信息流
+          </Button>
+        </Navbar.Section>
+      )}
     </Navbar>
   )
 }
