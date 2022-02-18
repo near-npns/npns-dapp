774055eb69946c7b47456b96eb8f0cb385ce895f
kula <kula@live.com>
2022-02-18 10:13:36 +0800

    feat: 增加首页

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,13 +1,14 @@
 import CreateChannelPage from '@/page/CreateChannel'
 import CreateMessagePage from '@/page/CreateMessage'
 import FeedPage from '@/page/Feed'
+import Index from '@/page/Index'
 import ListChannelPage from '@/page/ListChannel'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<FeedPage />} />
+      <Route path="/" element={<Index />} />
       <Route path="/createchannel" element={<CreateChannelPage />} />
       <Route path="/listchannel" element={<ListChannelPage />} />
       <Route path="/createmessage" element={<CreateMessagePage />} />
